017e36aa6ba3401ccb5c7852e0743ce9a25f272c
vanita22 <inesvanessapcm@gmail.com>
2017-04-25 14:02:55 -0300

    inicializando el programa

---
+++ main.js
@@ -0,0 +1,6 @@
+var mensajes= document.getElementsByTagName("mensaje");
+var respuesta = document.getElementById("respuesta");
+respuesta.innerHTML = "En ésta página hay " + (mensajes.length) + " mensaje."
+
+
+
